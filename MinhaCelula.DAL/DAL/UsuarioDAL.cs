d927f923dbeb2584953bde6e63c3d3a56824d4b7
Leandro Valentim <leandro.negresco@gmail.com>
2019-09-22 11:09:33 -0300

    Subir

---
+++ MinhaCelula.DAL/DAL/UsuarioDAL.cs
@@ -0,0 +1,20 @@
+﻿using MinhaCelula.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaCelula.DAL.DAL
+{
+    public class UsuarioDAL
+    {
+        public int NovoUsuario(Usuario us)
+        {
+            using (DatabaseContext contexto = new DatabaseContext())
+            {
+                return 0;
+            }
+        }
+    }
+}
