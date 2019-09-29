b5bccd9284ce62716c173210b38cfe77ffd17315
Leandro Valentim <leandro.negresco@gmail.com>
2019-09-29 12:39:23 -0300

    Controller Celulas

---
+++ MinhaCelula.WEB/Controllers/CelulasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MinhaCelula.WEB.Controllers
+{
+    public class CelulasController : Controller
+    {
+        // GET: Celulas
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
